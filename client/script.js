184d61351dc59d35f05340ea1deb05f9bde31fbc
tigju <tigju24@gmail.com>
2023-04-13 19:59:08 -0400

    navbar, ligh/dark mode

--- client/script.js
+++ client/script.js
@@ -3,6 +3,7 @@ import education from './assets/education.svg'
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
+const top_u = document.querySelector('#options')
 
 let loadInterval
 
@@ -35,11 +36,12 @@ function replaceURLs(message) {
 }
 
 function typeText(element, text) {
-    let index = 0
+    //let index = 0
     let formattedText = replaceURLs(text)
+
     let typed = new Typed(element, {
         strings: [formattedText],
-        typeSpeed: 20,
+        typeSpeed: 3,
         loop: false,
         showCursor: false
     })
@@ -89,8 +91,14 @@ function chatStripe(isAi, value, uniqueId) {
 const handleSubmit = async (e) => {
     e.preventDefault()
 
-    const data = new FormData(form)
-
+    let data = ''
+    if (form !== null) {
+        data = new FormData(form)
+    } else {
+        let select = form.firstElementChild
+        data = new FormData(select.value)
+    }
+    
     // user's chatstripe
     //chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
@@ -120,7 +128,7 @@ const handleSubmit = async (e) => {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            prompt: data.get('prompt')
+            prompt: `Give me ${data.get('prompt')}`
         })
     })
 
@@ -130,23 +138,84 @@ const handleSubmit = async (e) => {
     if (response.ok) {
         const data = await response.json()
         const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
-        console.log(parsedData)
         typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
+        // messageDiv.innerHTML = "Something went wrong"
+        messageDiv.innerHTML = err
+        // alert(err)
     }
 }
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
+if (form !== null) {
+    form.addEventListener('submit', handleSubmit)
+    form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         handleSubmit(e)
-    }
-})
-form.addEventListener('change', (e) => {
-    
+        }
+    })
+    form.addEventListener('change', (e) => {
         handleSubmit(e)
-})
+    })
+    // document.addEventListener('DOMContentLoaded', (e) => {
+    //     handleSubmit(e)
+        
+    // })
+
+// } else {
+//     top_u.addEventListener('change', (e) => {
+//         window.location = top_u.value
+//     })
+    // output.addEventListener('click', (e) => {
+    //     window.location = top_u.value
+    // })
+}
+
+if (window.location.pathname === '/more.html') {
+    document.addEventListener('DOMContentLoaded', (e) => {
+        handleSubmit(e)
+        
+    })
+}
+
+
+// navbar, light/dark mode
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light"),
+    searchToggle = document.querySelector(".searchToggle"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    siderbarClose = document.querySelector(".siderbarClose");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+    body.classList.add("dark");
+}
+
+// js code to toggle dark and light mode
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+
+    // js code to keep user selected mode even page refresh or file reopen
+    if (!body.classList.contains("dark")) {
+        localStorage.setItem("mode", "light-mode");
+    } else {
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
+
+
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click", e => {
+    let clickedElm = e.target;
+
+    if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+        nav.classList.remove("active");
+    }
+});
